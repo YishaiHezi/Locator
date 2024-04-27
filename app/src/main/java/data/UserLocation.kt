4bd356c0d31619e059ffe86a3f11b3bd3390fbcc
Yishai Hezi <yishai.hezi@moovit.com>
2024-04-27 17:50:43 +0300

    New data class: User, for recent searches

--- app/src/main/java/data/UserLocation.kt
+++ app/src/main/java/data/UserLocation.kt
@@ -4,5 +4,14 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 
+/**
+ * Represents a user.
+ */
+data class User(val name: String, val id: String)
+
+
+/**
+ * Represents a user location.
+ */
 @Parcelize
 data class UserLocation(val lat: Double, val lon: Double) : Parcelable
